49c05b79d759e92ce3a529c53c0f23505e840b67
DoubleYeah <31724867+DoubleYeah@users.noreply.github.com>
2018-06-24 18:56:15 +0800

    decorator

---
+++ decorator.js
@@ -0,0 +1,31 @@
+function MacBook() {
+	this.cost = function () {return 997;};
+	this.screenSize = function () {return 11.6};
+}
+
+//Derocator1
+function Memory(macbook){
+	var v = macbook.cost();
+	macbook.cost = function () {return v + 75;};
+}
+
+//Derocator2
+function Engraving(macbook){
+	var v = macbook.cost();
+	macbook.cost = function () {return v + 200;};
+}
+
+//Derocator3
+function Insurance(macbook){
+	var v = macbook.cost();
+	macbook.cost = function () {return v + 250;};
+}
+
+var mb = new MacBook();
+Memory(mb);
+Engraving(mb);
+Insurance(mb);
+
+console.log(mb.cost());
+
+console.log(mb.screenSize());
